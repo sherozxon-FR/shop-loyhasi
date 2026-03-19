b3477ef76a24942bf2776a698c9a94d3e7eafd88
sherozxon-coder <sherozeshmamatov697@gmail.com>
2026-03-19 13:50:47 +0500

    bazi  uzgarishlar

--- js/savat.js
+++ js/savat.js
@@ -1,64 +1,66 @@
-
-
-
-
-
 import { CARTITEM, SETLOCALSTORAGE } from "./index.js";
 
 const countSAVAT = document.querySelector(".countSAVAT");
 const savatContainer = document.querySelector(".savat_container");
 const totalElement = document.querySelector(".TOTAL");
 
-
-
-
 function ShowSavat() {
     savatContainer.innerHTML = '';
     let total = 0;
-    CARTITEM.forEach((item, index) => {
-        savatContainer.innerHTML += ` 
-        <div class="box">
-                <div class="box-img">
-                    <div class="imageC">
-                        <img src="../${item.img}" alt="${item.name}">
-                    </div>
 
-                    <div class="info">
-                        <h4> ${item.name}</h4>
-                        <div class="itemPQ">
-                            <span> $${item.price} x <b>${item.quantity}</b> </span>
-                        </div>
-                    </div>
+    CARTITEM.forEach((item, index) => {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.innerHTML = `
+            <div class="box-img">
+                <div class="imageC">
+                    <img src="../${item.img}" alt="${item.name}">
                 </div>
-                <div class="btn">
-                    <button>-</button>
-                    <button>+</button>
-                    <button class="remove">x</button>
+                <div class="info">
+                    <h4>${item.name}</h4>
+                    <div class="itemPQ">
+                        <span>$${item.price} x <b>${item.quantity}</b></span>
+                    </div>
                 </div>
             </div>
-        
+            <div class="btn">
+                <button class="minus">-</button>
+                <button class="plus">+</button>
+                <button class="remove">x</button>
+            </div>
         `;
+
+        // Minus tugmasi
+        box.querySelector('.minus').addEventListener('click', () => {
+            if (item.quantity > 1) {
+                CARTITEM[index].quantity--;
+            } else {
+                CARTITEM.splice(index, 1);
+            }
+            SETLOCALSTORAGE();
+            ShowSavat();
+        });
+
+        // Plus tugmasi
+        box.querySelector('.plus').addEventListener('click', () => {
+            CARTITEM[index].quantity++;
+            SETLOCALSTORAGE();
+            ShowSavat();
+        });
+
+        // Remove tugmasi
+        box.querySelector('.remove').addEventListener('click', () => {
+            CARTITEM.splice(index, 1);
+            SETLOCALSTORAGE();
+            ShowSavat();
+        });
+
+        savatContainer.appendChild(box);
         total += item.price * item.quantity;
     });
 
     countSAVAT.textContent = CARTITEM.length;
     totalElement.textContent = `TOTAL: $${total.toFixed(2)}`;
-
-    // Event listenerlarni har safar yangilash
-    RemoveButtons();
-}
-
-function RemoveButtons() {
-    const removeButtons = document.querySelectorAll('.remove');
-    removeButtons.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            CARTITEM.splice(index, 1);
-            SETLOCALSTORAGE();
-            ShowSavat();
-        });
-    });
 }
 
 ShowSavat();
-
-
